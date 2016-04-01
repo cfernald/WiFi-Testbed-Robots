f336d191254236185f8d1f2aa1f1364c16012d2b
TylerTWalker <tyler.walker@utexas.edu>
2016-04-01 13:48:09 -0500

    squaremaster

---
+++ SquareGenerator.py
@@ -0,0 +1,21 @@
+import sys, math
+def genCircle():
+	f = open('waypoints.csv','w')
+	radiusOfEarth = 6371.0
+	start_lat =float( sys.argv[1])
+	start_lon = float(sys.argv[2])
+	radius = float(sys.argv[3])
+	start_lat = math.radians(start_lat)
+	start_lon = math.radians(start_lon)
+	for i in range(0,4):
+		heading = math.radians(45+ 90*i)	
+		destLat = math.asin(math.sin(start_lat)*math.cos(radius/radiusOfEarth) + math.cos(start_lat)*math.sin(radius/radiusOfEarth)*math.cos(heading))
+		destLon = start_lon + math.atan2(math.sin(heading)*math.sin(radius/radiusOfEarth)*math.cos(start_lat), math.cos(radius/radiusOfEarth) - math.sin(start_lat)*math.sin(destLat))
+		destLat = math.degrees(destLat);
+		destLon = math.degrees(destLon);
+		destLon = (destLon+540.0)%360.0-180.0;	
+		output = str(destLat) + ', ' + str(destLon) + '\n'
+		f.write(output)
+
+if __name__ == "__main__":
+    genCircle()
