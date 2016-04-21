7d6ba1bdc46aaea56a8a26244a3b0933206aa1f0
awesome sauce <taekwondokid@gmail.com>
2016-04-21 17:09:12 -0500

    added 3 compass stuff

---
+++ autobots/catkin_ws/src/proteus3_compass_hydro/Arduino/Compass3/compassavg.ino
@@ -0,0 +1,80 @@
+/**
+ * Accesses the HMC6343 compass for the heading, pitch, and roll at 10Hz.
+ * Packages the data in a Proteus message and prints it to the serial port.
+ *
+ * Two LEDs are used:
+ *  - pin 6, green, on upon initialization
+ *  - pin 7, red, blinked each time the compass is accessed.
+ *
+ * The message transmitted:
+ *  [proteus begin (1 byte)][heading (2 bytes)][pitch (2 bytes)][roll (2 bytes)][checksum (1 byte)]
+ *
+ * The checksum is the XOR of the other bytes in the message.
+ *
+ * @author Chien-Liang Fok
+ */
+#include <Wire.h>
+
+/**
+ * Define the constants.
+ */
+#define HMC6343_ADDRESS            0x19
+#define HMC6343_HEADING_REG        0x50
+#define PROTEUS_BEGIN              0x24
+
+int GREEN_LED = 6;
+int RED_LED = 7;
+
+byte buf[8]; // A buffer for holding the message
+
+void setup() {
+  buf[0] = PROTEUS_BEGIN;
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  Wire.begin(); // Initialize the I2C bus
+  Serial.begin(115200); // Initialize the serial bus
+  digitalWrite(GREEN_LED, HIGH); // Turn on the green LED to incate system started
+}
+
+void loop() {
+  Wire.beginTransmission(HMC6343_ADDRESS);    // Start communicating with the HMC6343 compasss
+  Wire.write(HMC6343_HEADING_REG);             // Send the address of the register that we want to read
+  Wire.endTransmission();
+
+  Wire.requestFrom(HMC6343_ADDRESS, 6);    // Request six bytes of data from the HMC6343 compasss
+  while(Wire.available() < 1);             // Busy wait while there is no byte to receive
+
+  buf[1] = Wire.read();              // Reads in the bytes and convert them into proper degree units.
+  buf[2] = Wire.read();
+  //float heading = ((highByte << 8) + lowByte) / 10.0; // the heading in degrees
+
+  buf[3] = Wire.read();
+  buf[4] = Wire.read();
+  //float pitch = ((highByte << 8) + lowByte) / 10.0;   // the pitch in degrees
+
+  buf[5] = Wire.read();
+  buf[6] = Wire.read();
+  //float roll = ((highByte << 8) + lowByte) / 10.0;    // the roll in degrees
+
+  // compute the checksum
+  buf[7] = 0;
+  for (int i = 0; i < 7; i++) {
+    buf[7] ^= buf[i];
+  }
+
+  Serial.write(buf, 8);
+
+  /*Serial.print("Heading=");             // Print the sensor readings to the serial port.
+  Serial.print(heading);
+  Serial.print(", Pitch=");
+  Serial.print(pitch);
+  Serial.print(", Roll=");
+  Serial.println(roll);*/
+
+  digitalWrite(RED_LED, HIGH);
+  delay(20);
+  digitalWrite(RED_LED, LOW);
+  delay(80);
+
+  //delay(100); // Do this at approx 10Hz
+}
---
+++ autobots/catkin_ws/src/proteus3_compass_hydro/src/3compass.cpp
@@ -0,0 +1,201 @@
+/**
+ * This ROS node does the following:
+ *
+ * 1. Reads in compass data from a serial port
+ * 2. Packages up the data within a message
+ * 3. Publishes the message through ROS topic /compass/measurement
+ *
+ * The compass is an HMC6343 with Tilt Compensation device.
+ * Thus it reports the heading, pitch, and roll.
+ *
+ * @author Chien-Liang Fok
+ */
+#include <string>
+#include <iostream>
+#include <cstdio>
+
+#include "serial/serial.h"
+
+#include "ros/ros.h"
+#include "proteus3_compass_hydro/CompassMsg.h"
+#include <sstream>
+
+#define PROTEUS_START 0x24 // a special byte to indicate the start of a message
+#define COMPASS_MESSAGE_SIZE 8 // the size of the compass message in bytes
+
+using std::string;
+using std::exception;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int run(int argc, char **argv) {
+  ros::init(argc, argv, "compass");
+
+  // This node handle's namespace is "compass".
+  // See:  http://www.ros.org/wiki/roscpp/Overview/NodeHandles#Namespaces
+  // IMPORTANT:  the correct syntax to publish to the compass namespace is ->  ros::NodeHandle node("compass")
+  //      This was changed by Pedro Santacruz on 3/18/2014
+  ros::NodeHandle node("compass");
+
+  // Get the parameters
+  // See: http://www.ros.org/wiki/roscpp/Overview/Parameter%20Server
+  std::string port;
+  node.param<std::string>("port", port, "/dev/ttyACM0");
+  cout << "Port: " << port << endl;
+
+  int baud = 115200;
+  node.param<int>("baud", baud, 115200);
+  cout << "Baud: " << baud << endl;
+
+  serial::Serial my_serial(port, baud, serial::Timeout::simpleTimeout(1000));
+
+  cout << "Is the serial port open?";
+  if(my_serial.isOpen())
+    cout << " Yes." << endl;
+  else
+    cout << " No." << endl;
+
+  /*
+   * Tell ROS that this node is going to publish messages on topic "measurement".
+   * The buffer size is 1000, meaning up to 1000 messages will be stored
+   * before throwing any away.
+   */
+  ros::Publisher chatter_pub = node.advertise<proteus3_compass_hydro::CompassMsg>("measurement", 1000);
+  /*
+   * Loop at 20Hz.
+   */
+  ros::Rate loop_rate(20);
+  uint8_t *buff = new uint8_t[COMPASS_MESSAGE_SIZE];
+  bool readMsg = false;
+
+  while (ros::ok()) {
+    if (my_serial.available() >= 8) {
+      /* Read one byte from the serial port.
+       * If it is a PROTEUS_START byte, read
+       * the remaining bytes in the message
+       * then process the message.
+       */
+
+
+      size_t numBytes = my_serial.read(buff, 1);
+      if (numBytes == 1) {
+        if (buff[0] == PROTEUS_START) {
+          readMsg = true;
+          numBytes = my_serial.read(&buff[1], COMPASS_MESSAGE_SIZE - 1);
+
+          if (numBytes == COMPASS_MESSAGE_SIZE - 1) {
+
+            // print the message
+            /*printf("Read message: ");
+            for (uint8_t i = 0; i < COMPASS_MESSAGE_SIZE; i++) {
+              printf("0x%x ", buff[i]);
+            }
+            printf("\n");*/
+
+            /* Compute the checksum, which is the XOR of all the bytes
+             * in the message except for the last one
+             */
+            uint8_t checksum = 0;
+            for (uint8_t i = 0; i < COMPASS_MESSAGE_SIZE - 1; i++) {
+              checksum ^= buff[i];
+            }
+            //printf("Checksum = 0x%x\n", checksum);
+
+            if (checksum == buff[7]) {
+              //printf("Checksum matched!\n");
+              float headingDeg1 = ((uint16_t)((buff[1] << 8) + buff[2])) / 10.0; // heading in degrees (0 to 360)
+              float headingDeg2 = ((int16_t)((buff[3] << 8) + buff[4])) / 10.0; // pitch in degrees (-90 to 90)
+              float headingDeg3 =  ((int16_t)((buff[5] << 8) + buff[6])) / 10.0; // roll in degrees (-90 to 90)
+              float pitchDeg = ((int16_t)((buff[3] << 8) + buff[4])) / 10.0; // pitch in degrees (-90 to 90)
+              float rollDeg =  ((int16_t)((buff[5] << 8) + buff[6])) / 10.0; // roll in degrees (-90 to 90)
+              float heading [3] = {headingDeg1,headingDeg2,headingDeg3};
+              // From the way the compass is mounted on the Proteus III,
+              // the heading measurement needs to be rotated by -90 degrees.
+              cout << "Heading1: " << headingDeg1 <<endl;
+              cout << "Heading2: " << headingDeg2 <<endl;
+              cout << "Heading3: " << headingDeg3 <<endl;
+              for(int i=0; i<3;i++)
+              {
+                heading[i] -= 90;
+                if (heading[i] < 0)
+                  heading[i] = 360 + heading[i];
+
+                // Convert the heading from 0-360 to -180 to 180
+                if (heading[i] < 180)
+                  heading[i] *= -1;
+                else
+                  heading[i] = 360 - heading[i]; //180 - (headingDeg - 180)
+              }
+              //compute differences
+              float diff[3];
+              for(int i=0;i<3;i++)
+              {
+                for(int j=1;j<3;j++)
+                  diff[0] = heading[i]-heading[j];
+              }
+              //find the smalles
+              float smalles =999;
+              for(int i=0;i<3;i++)
+              {
+                if(diff[i]<smalles)
+                  smalles = i;
+              }
+              float headingDeg;
+              switch(smalles)
+                case 0:
+                  headingDeg = heading[0]+heading[1];
+                  headingDeg = headingDeg/2;
+                  break;
+                case 1:
+                  headingDeg = heading[0]+heading[2];
+                  headingDeg = headingDeg/2;
+                  break;
+                case 2:
+                  headingDeg = heading[2]+heading[3];
+                  headingDeg = headingDeg/2;
+                  break;
+              cout << "Heading: " << headingDeg << ", Pitch: " << pitchDeg << ", Roll: " << rollDeg << endl;
+            //  cout <<  headingDeg <<  pitchDeg <<  rollDeg << endl;
+
+
+              proteus3_compass_hydro::CompassMsg msg;
+              msg.heading = headingDeg;
+              msg.pitch = pitchDeg;
+              msg.roll = rollDeg;
+
+              //ROS_INFO("%s", msg.data.c_str());
+
+              chatter_pub.publish(msg);
+
+            } else {
+              printf("Checksum missmatch! (0x%x != 0x%x)\n", checksum, buff[7]);
+            }
+          } else {
+            printf("Failed to read entire message (%i of %i bytes)\n", numBytes, COMPASS_MESSAGE_SIZE - 1);
+          }
+        } else {
+          printf("First byte not start byte, discard it...\n");
+          readMsg = false;
+        }
+      }
+    }
+
+    ros::spinOnce();
+
+    // only sleep if a message was successfully read
+    if (readMsg)
+      loop_rate.sleep();
+  }
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  try {
+    return run(argc, argv);
+  } catch (exception &e) {
+    cerr << "Unhandled Exception: " << e.what() << endl;
+  }
+  return 0;
+}
